a16b1c1b562a65cb95edd2da93230fd597efc87f
Nurio34 <nurioonsoftware@gmail.com>
2023-10-27 23:40:29 +0300

    Update on tailwind

--- postcss.config.js
+++ postcss.config.js
@@ -3,72 +3,49 @@ if (process.env.NODE_ENV === 'production') {
 
     module.exports = {
 
-      plugins: {
-        "postcss-preset-env":{
-            stage: 3,
-            features: {
-              'nesting-rules': true
-            },
-            env : "development",
-            browsers : "last 4 versions",
-        },
-        cssnano: {},
-        'rucksack-css': {},
-      },
+      plugins: [
+        require("tailwindcss")("tailwind.config.js"),
+        require("postcss-preset-env")({
+          stage: 3,
+          features: {
+            'nesting-rules': true,
+          },
+          env: 'development',
+          browsers: 'last 4 versions',
+        }),
+        require("cssnano"),
+        require("rucksack-css")
+
+      ],
     };
 
   } else {
 
     module.exports = {
 
-      plugins: {
-        "postcss-preset-env":{
-            stage: 3,
-            features: {
-              'nesting-rules': true
-            },
-            env : "development",
-            browsers : "last 4 versions",
+      plugins: [
+        require("tailwindcss")("tailwind.config.js"),
+        require("postcss-preset-env")({
+        stage: 3,
+        features: {
+          'nesting-rules': true,
         },
-        'rucksack-css': {},
-      },
+        env: 'development',
+        browsers: 'last 4 versions',
+      }),
+        require("cssnano"),
+        require("rucksack-css")
+      ]
     };
   }
 
-// //   if (process.env.NODE_ENV === 'production') {
-// //     module.exports = {
-// //       plugins: {
-// //         autoprefixer: {},
-// //         cssnano: {},
-// //         'rucksack-css': {},
-// //       },
-// //     };
-// //   } else {
-// //     module.exports = {
-// //       plugins: {
-// //         autoprefixer: {},
-// //         'rucksack-css': {},
-// //       },
-// //     };
-// //   }
-postcssOptions : {
-  plugins : [
-      [
-          "postcss-preset-env",
-          {
-              stage: 3,
-              features: {
-                'nesting-rules': true
-              },
-              env : "production",
-              browsers : "last 4 versions",
-          }
-      ],
-      [
-          "cssnano"
-      ],
-      [
-          "rucksack-css"
-      ]
-  ]
-}
+        // "postcss-preset-env":{
+        //     stage: 3,
+        //     features: {
+        //       'nesting-rules': true
+        //     },
+        //     env : "development",
+        //     browsers : "last 4 versions",
+        // },
+        // cssnano: {},
+        // 'rucksack-css': {},
