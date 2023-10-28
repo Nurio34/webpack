46bb6bb90b1db851f3414e43132de493c9e1e4d1
Nurio34 <nurioonsoftware@gmail.com>
2023-10-28 05:17:30 +0300

    Day5-Night

--- postcss.config.js
+++ postcss.config.js
@@ -1,51 +1,42 @@
-
-if (process.env.NODE_ENV === 'production') {
-
+if (process.env.NODE_ENV === "production") {
     module.exports = {
-
-      plugins: [
-        require("tailwindcss")("tailwind.config.js"),
-        require("postcss-preset-env")({
-          stage: 3,
-          features: {
-            'nesting-rules': true,
-          },
-          env: 'development',
-          browsers: 'last 4 versions',
-        }),
-        require("cssnano"),
-        require("rucksack-css")
-
-      ],
+        plugins: [
+            require("tailwindcss")("tailwind.config.js"),
+            require("postcss-preset-env")({
+                stage: 3,
+                features: {
+                    "nesting-rules": true,
+                },
+                env: "development",
+                browsers: "last 4 versions",
+            }),
+            require("cssnano"),
+        ],
     };
-
-  } else {
-
+} else {
     module.exports = {
-
-      plugins: [
-        require("tailwindcss")("tailwind.config.js"),
-        require("postcss-preset-env")({
-        stage: 3,
-        features: {
-          'nesting-rules': true,
-        },
-        env: 'development',
-        browsers: 'last 4 versions',
-      }),
-        require("cssnano"),
-        require("rucksack-css")
-      ]
+        plugins: [
+            require("tailwindcss")("tailwind.config.js"),
+            require("postcss-preset-env")({
+                stage: 3,
+                features: {
+                    "nesting-rules": true,
+                },
+                env: "development",
+                browsers: "last 4 versions",
+            }),
+            require("cssnano"),
+        ],
     };
-  }
+}
 
-        // "postcss-preset-env":{
-        //     stage: 3,
-        //     features: {
-        //       'nesting-rules': true
-        //     },
-        //     env : "development",
-        //     browsers : "last 4 versions",
-        // },
-        // cssnano: {},
-        // 'rucksack-css': {},
+// "postcss-preset-env":{
+//     stage: 3,
+//     features: {
+//       'nesting-rules': true
+//     },
+//     env : "development",
+//     browsers : "last 4 versions",
+// },
+// cssnano: {},
+// 'rucksack-css': {},
