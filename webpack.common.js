0b2585bcc911f4440a760133fd909c6db74a3ab8
Nurio34 <nurioonsoftware@gmail.com>
2023-11-24 16:14:51 +0300

    Still try to deploy

--- webpack.common.js
+++ webpack.common.js
@@ -47,7 +47,7 @@ module.exports = {
     ],
 
     output: {
-        path: path.resolve(__dirname, "dist"),
+        path: path.resolve(__dirname, "build"),
         filename: "[name].js",
         clean: true,
         assetModuleFilename: "[name][ext]"
